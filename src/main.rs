624add040fb2c707011f5aca22d288d8bcda823b
OgGhostJelly <johnnycambodia09@gmail.com>
2023-11-14 10:35:01 +0700

    Big update

--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,49 @@
-mod game;
-mod utils;
+mod tree;
 
-use game::event::{Action, Event};
-use game::player::Player;
-//use rand::seq::SliceRandom;
-use std::collections::HashMap;
-use std::io;
-use utils::tree::NodePath;
+#[derive(Clone, Debug)]
+struct Action<'a> {
+    text: &'a str,
+}
 
-use crate::utils::tree::Node;
+#[derive(Clone, Debug)]
+struct Event<'a> {
+    text: &'a str,
+    actions: Vec<Action<'a>>,
+}
 
 fn main() {
-    //let mut rng = rand::thread_rng();
-
-    let mut event = &Event::new(
-        "Hello, welcome to my game!",
-        vec![
-            Action::new("", |_| { &["abc"] } ),
-            /*Action {
-                text: "",
-                action: |_player| { const P: NodePath<'_> = NodePath::new(&[""]); &P }
-            }*/
-        ],
-        |_player| true,
-        HashMap::from([(
-            "goblin",
-            Event::new(
-                "A goblin appears!",
-                vec![],
-                |_player| true,
-                HashMap::from([]),
+    /*
+    tree::a();
+    let root: tree::PathedNode<Event> = tree::PathedNode::create(
+        Event {
+            text: "Welcome!",
+            actions: [Action { text: "Attack" }, Action { text: "Escape" }].to_vec(),
+        },
+        [
+            tree::PathedNode::create(
+                Event {
+                    text: "You attacked!",
+                    actions: [Action { text: "Jump" }, Action { text: "Dance" }].to_vec(),
+                },
+                [
+                    tree::PathedNode::new(Event {
+                        text: "You jumped",
+                        actions: [].to_vec(),
+                    }),
+                    tree::PathedNode::new(Event {
+                        text: "You danced",
+                        actions: [].to_vec(),
+                    }),
+                ]
+                .to_vec(),
             ),
-        )]),
+            tree::PathedNode::new(Event {
+                text: "You escaped",
+                actions: [].to_vec(),
+            }),
+        ]
+        .to_vec(),
     );
 
-    let mut player = Player::new(50, 3, 10);
-
-    loop {
-        // If event is not ok, continue
-        if !event.init(&mut player) {
-            continue;
-        }
-
-        // Print text and player actions
-        println!("{}", event.text());
-
-        for (index, action) in event.actions().iter().enumerate() {
-            println!("({0}){1}", index + 1, action.text())
-        }
-
-        // Get player input
-        let mut action: String = String::new();
-
-        io::stdin()
-            .read_line(&mut action)
-            .expect("Failed to read line.");
-
-        let action: usize = match action.trim().parse::<usize>() {
-            Ok(v) => match v.checked_sub(1) {
-                Some(v) => v,
-                None => continue,
-            },
-            Err(_) => continue,
-        };
-
-        event = match event.get_node(match event.act(&mut player, action) {
-            Some(v) => v,
-            None => continue,
-        }) {
-            Some(v) => v,
-            None => continue,
-        }
-    }
+    println!("{:#?}", root);*/
 }
