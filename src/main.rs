649a2c4e7579cb25e9cff6f1b9d99d1657a8b842
OgGhostJelly <johnnycambodia09@gmail.com>
2024-09-11 14:57:36 +0700

    Better frame rate handling

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,12 @@ use sdl2::event::Event;
 mod board;
 mod game_context;
 
+// Optimization idea
+// Generate multiple variations of board and then combine with par iter
+// Call them `BoardPiece`s a board piece is kinda like a slice with an offset and len but for 2d space
+// BoardPieces can be combined
+// Generate many board pieces from chunks and then combine them together in parellel
+
 fn main() -> Result<(), String> {
     #[cfg(debug_assertions)]
     eprintln!("{}", "WARN: you are running in debug. debug is slower and less optimized than release. to run in release do `cargo run --release`".yellow());
@@ -33,15 +39,27 @@ fn main() -> Result<(), String> {
     let draw_board_ref = draw_board.clone();
 
     std::thread::spawn(move || {
+        let mut last_update = Instant::now();
+
         loop {
             let old_board = board;
+            let before = Instant::now();
             board = old_board.tick();
+            println!("Generated {:?} tiles in {:?}", old_board.alive_cells().len(), Instant::now() - before);
             
             let mut cached_board = draw_board_ref.lock().unwrap();
             *cached_board = old_board;
             drop(cached_board);
-
-            ::std::thread::sleep(Duration::from_nanos(16000000));
+            
+            let delta = Instant::now() - last_update;
+            last_update = Instant::now();
+            
+            // Cap framerate to 16ms or 60fps
+            let frame_time = Duration::from_nanos(16000000);
+            let excess = frame_time.checked_sub(delta);
+            if let Some(excess) = excess {
+                ::std::thread::sleep(excess)
+            }
         }
     });
 
