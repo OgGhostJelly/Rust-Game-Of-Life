f3814ac243b9bb429015279dede54650c6ff86b2
OgGhostJelly <johnnycambodia09@gmail.com>
2024-09-11 07:47:55 +0700

    Better testing and performance tweaks (no graphics)

--- src/board.rs
+++ src/board.rs
@@ -1,20 +1,41 @@
+use std::{collections::HashSet, fmt::{self, Write}};
+
 use rand::{rngs::ThreadRng, Rng};
 
-pub const W: usize = 250;
-pub const H: usize = 250;
-
-#[derive(PartialEq, Eq)]
-#[derive(Debug)]
-pub struct Board {
+#[derive(Clone)]
+pub struct Board<const W: usize, const H: usize> {
     cells: [[Cell; W]; H],
     alive_cells: Vec<(usize, usize)>,
 }
 
-impl Board {
+impl<const W: usize, const H: usize> Board<W, H> {
     pub fn new() -> Self {
         Self {
-            cells: [[Cell::new(false, 0); W]; H],
+            cells: [[Cell::empty(); W]; H],
             alive_cells: Vec::new(),
+        }
+    }
+
+    pub fn from_cells(cells: &[&[usize]]) -> Self {
+        let mut board = Self::new();
+
+        for (y, row) in cells.into_iter().enumerate() {
+            for (x, cell) in row.into_iter().enumerate() {
+                if *cell != 0 {
+                    board.make_alive(x, y);
+                }
+            }
+        }
+
+
+
+        board
+    }
+
+    pub fn with_alive_capacity(capacity: usize) -> Self {
+        Self {
+            cells: [[Cell::empty(); W]; H],
+            alive_cells: Vec::with_capacity(capacity),
         }
     }
 
@@ -26,7 +47,7 @@ impl Board {
     }
 
     pub fn rand(rng: &mut ThreadRng, p: f64) -> Self {
-        let mut board = Board::new();
+        let mut board = Self::with_alive_capacity((H * W) / 2);
 
         for y in 0..H {
             for x in 0..W {
@@ -44,11 +65,21 @@ impl Board {
     }
     
     pub fn tick(self) -> Self {
-        let mut board = Self::new();
+        let mut board = Self::with_alive_capacity(self.alive_cells.len());
 
         for (x, y) in self.alive_cells.into_iter() {
-            if self.cells[y][x].next_state() {
+            if self.cells[y][x].next_alive_state() {
                 board.make_alive(x, y);
+            }
+            
+            for (x, y) in get_adjacents(x, y) {
+                if x >= W || y >= H {
+                    continue;
+                }
+
+                if self.cells[y][x].is_dead() && board.cells[y][x].is_dead() && self.cells[y][x].next_dead_state() {
+                    board.make_alive(x, y);
+                }
             }
         }
 
@@ -72,6 +103,57 @@ impl Board {
     }
 }
 
+impl<const W: usize, const H: usize> PartialEq for Board<W, H> {
+    fn eq(&self, other: &Self) -> bool {
+        if self.alive_cells.len() != other.alive_cells.len() {
+            return false;
+        }
+        
+        let map: HashSet<&(usize, usize)> = HashSet::from_iter(self.alive_cells.iter());
+
+        for key in &other.alive_cells {
+            if !map.contains(key) {
+                return false;
+            }
+        }
+
+        if self.cells.len() != other.cells.len() {
+            return false;
+        }
+
+        self.cells.iter()
+            .zip(other.cells.iter())
+            .map(|(a, b)| {
+                if a.len() != b.len() {
+                    return false;
+                }
+
+                a.iter()
+                    .zip(b.iter())
+                    .map(|(a, b)| a == b)
+                    .all(|v| v)
+            })
+            .all(|v| v)
+    }
+}
+
+impl<const W: usize, const H: usize> fmt::Debug for Board<W, H> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for row in &self.cells {
+            f.write_char('\n')?;
+
+            for cell in row {
+                write!(f, "({}, {}), ",
+                    if cell.is_alive() {"t"} else {"f"},
+                    cell.neighbour_count()
+                )?;
+            }
+        }
+        
+        Ok(())
+    }
+}
+
 fn get_adjacents(to_x: usize, to_y: usize) -> impl Iterator<Item = (usize, usize)> {
     (-1isize..=1).map(move |y|
         (-1isize..=1).filter_map(move |x| {
@@ -85,8 +167,7 @@ fn get_adjacents(to_x: usize, to_y: usize) -> impl Iterator<Item = (usize, usize
 
 /// A single game of life cell.
 #[derive(Clone, Copy)]
-#[derive(PartialEq, Eq)]
-#[derive(Debug)]
+#[derive(PartialEq, Eq, Hash)]
 pub struct Cell(i8);
 
 impl Cell {
@@ -98,6 +179,10 @@ impl Cell {
         debug_assert!(neighbour_count <= 8, "integer overflow: neighbour_count (is {}) <= 8", neighbour_count);
         debug_assert_eq!(neighbour_count & !Self::NEIGHBOUR, 0, "integer underflow: neighbour_count (is {}) cannot be negative", neighbour_count);
         Self(neighbour_count | if is_alive { Self::LIVE } else { 0 })
+    }
+
+    pub fn empty() -> Self {
+        Self::new(false, 0)
     }
 
     #[inline]
@@ -127,12 +212,23 @@ impl Cell {
 
     #[inline]
     pub fn next_state(self) -> bool {
-        let neighbour_count = self.neighbour_count();
         if self.is_alive() {
-            neighbour_count >= 2 && neighbour_count <= 3
+            self.next_alive_state()
         } else {
-            neighbour_count == 3
+            self.next_dead_state()
         }
+    }
+
+    #[inline]
+    pub fn next_alive_state(self) -> bool {
+        let neighbour_count = self.neighbour_count();
+        neighbour_count >= 2 && neighbour_count <= 3
+    }
+
+    #[inline]
+    pub fn next_dead_state(self) -> bool {
+        let neighbour_count = self.neighbour_count();
+        neighbour_count == 3
     }
 }
 
@@ -211,24 +307,215 @@ mod tests {
     }
 
     #[test]
+    fn test_from_cells() {
+        let mut expected = Board::<3, 3>::new();
+        expected.make_alive(1, 0);
+        expected.make_alive(1, 1);
+        expected.make_alive(1, 2);
+
+        let board = Board::from_cells(&[
+            &[0, 1, 0],
+            &[0, 1, 0],
+            &[0, 1, 0],
+        ]);
+
+        assert_eq!(expected, board);
+
+        let mut expected = Board::<3, 3>::new();
+        expected.make_alive(0, 1);
+        expected.make_alive(1, 1);
+        expected.make_alive(2, 1);
+
+        let board = Board::from_cells(&[
+            &[0, 0, 0],
+            &[1, 1, 1],
+            &[0, 0, 0],
+        ]);
+
+        assert_eq!(expected, board);
+    }
+
+    #[test]
+    fn test_board_tick() {
+        let board: Board<3, 3> = Board::from_cells(&[
+            &[0, 1, 0],
+            &[0, 1, 0],
+            &[0, 1, 0],
+        ]);
+
+        let expected = Board::from_cells(&[
+            &[0, 0, 0],
+            &[1, 1, 1],
+            &[0, 0, 0],
+        ]);
+
+        assert_eq!(board.tick(), expected)
+    }
+
+    #[test]
+    fn test_partial_eq() {
+        assert_eq!(
+            Board::<3, 3>::from_cells(&[
+                &[1, 1, 0],
+                &[0, 1, 1],
+                &[0, 1, 0],
+            ]),
+            Board::from_cells(&[
+                &[1, 1, 0],
+                &[0, 1, 1],
+                &[0, 1, 0],
+            ]),
+        );
+
+        assert_ne!(
+            Board::<3, 3>::from_cells(&[
+                &[0, 1, 1],
+                &[0, 1, 0],
+                &[1, 1, 1]
+            ]),
+            Board::from_cells(&[
+                &[0, 1, 1],
+                &[0, 1, 1],
+                &[1, 1, 1]
+            ]),
+        );
+
+        let board = Board::<3, 3>::from_cells(&[
+            &[0, 1, 1],
+            &[0, 1, 0],
+            &[1, 1, 1]
+        ]);
+
+        let expected = {
+            let mut board = board.clone();
+            board.alive_cells.pop();
+            board
+        };
+
+        assert_ne!(board, expected);
+
+        let board = Board::<3, 3>::from_cells(&[
+            &[0, 1, 1],
+            &[0, 1, 0],
+            &[1, 1, 1]
+        ]);
+
+        let expected = {
+            let mut board = board.clone();
+            board.alive_cells.pop();
+            board.alive_cells.push((0, 0));
+            board
+        };
+
+        assert_ne!(board, expected);
+    }
+
+    fn test_simulation<const W: usize, const H: usize>(steps: &[&[&[usize]]]) {
+        let (initial, steps) = (&steps[0], &steps[1..]);
+        let mut board = Board::<W, H>::from_cells(initial);
+        
+        for next in steps.into_iter() {
+            board = board.tick();
+
+
+            let expected: Board<W, H> = Board::from_cells(next);
+
+            for row in &board.cells {
+                print!("[");
+
+                for cell in row {
+                    print!("Cell({}, {})",
+                    if cell.is_alive() { "t" } else { "f" },
+                    cell.neighbour_count(),
+                );
+
+                    print!(", ");
+                }
+                
+                println!("]")
+            }
+
+            println!("----");
+
+            for row in &expected.cells {
+                print!("[");
+
+                for cell in row {
+                    print!("Cell({}, {})",
+                    if cell.is_alive() { "t" } else { "f" },
+                    cell.neighbour_count(),
+                );
+
+                    print!(", ");
+                }
+                
+                println!("]")
+            }
+
+            println!("cells: {:?}", board.cells == expected.cells);
+            println!("alive_cells: {:?}", board.alive_cells == expected.alive_cells);
+
+            assert_eq!(board, expected)
+        }
+    }
+
+    #[test]
     fn test_blinker() {
-        // . # .
-        // . # .
-        // . # .
-        let mut board = Board::new();
-        board.make_alive(1, 0);
-        board.make_alive(1, 1);
-        board.make_alive(1, 2);
+        test_simulation::<3, 3>(&[
+            &[
+                &[0, 1, 0],
+                &[0, 1, 0],
+                &[0, 1, 0],
+            ],
+            &[
+                &[0, 0, 0],
+                &[1, 1, 1],
+                &[0, 0, 0],
+            ],
+            &[
+                &[0, 1, 0],
+                &[0, 1, 0],
+                &[0, 1, 0],
+            ],
+            &[
+                &[0, 0, 0],
+                &[1, 1, 1],
+                &[0, 0, 0],
+            ],
+        ]);
+    }
 
-        // . . .
-        // # # #
-        // . . .
-        let expected = Board::new();
-        board.make_alive(0, 1);
-        board.make_alive(1, 1);
-        board.make_alive(2, 1);
-
-        let board = board.tick();
-        assert_eq!(board, expected);
+    #[test]
+    fn test_glider() {
+        test_simulation::<4, 4>(&[
+            &[
+                &[1, 0, 0],
+                &[0, 1, 1],
+                &[1, 1, 0],
+            ],
+            &[
+                &[0, 1, 0],
+                &[0, 0, 1],
+                &[1, 1, 1],
+            ],
+            &[
+                &[0, 0, 0],
+                &[1, 0, 1],
+                &[0, 1, 1],
+                &[0, 1, 0],
+            ],
+            &[
+                &[0, 0, 0],
+                &[0, 0, 1],
+                &[1, 0, 1],
+                &[0, 1, 1],
+            ],
+            &[
+                &[0, 0, 0, 0],
+                &[0, 1, 0, 0],
+                &[0, 0, 1, 1],
+                &[0, 1, 1, 0],
+            ],
+        ])
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,19 @@
 use std::{io::stdin, time::Instant};
 
 use board::Board;
+use colored::Colorize;
 use rand::thread_rng;
 
 mod board;
 
 fn main() {
-    let mut board = Board::rand(&mut thread_rng(), 0.5);
+    #[cfg(debug_assertions)]
+    eprintln!("{}", "WARN: you are running in debug. debug is slower and less optimized than release. to run in release do `cargo run --release`".yellow());
     
+    let mut board: Board::<250, 250> = Board::rand(&mut thread_rng(), 0.5);
+
     loop {
-        let cells = board.cells();
-        
-        /*{
-            let lock = stdout().lock();
-            
-            for row in cells {
-                for cell in row {
-                    if cell.is_alive() {
-                        print!("#")
-                    } else {
-                        print!(".")
-                    }
-                }
-        
-                println!()
-            }
-
-            drop(lock)
-        }*/
-
         let _ = stdin().read_line(&mut String::new());
-
         let previous = Instant::now();
         board = board.tick();
         println!("Generated in {:?}", (Instant::now() - previous).as_millis());
